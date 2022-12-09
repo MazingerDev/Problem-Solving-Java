bb350b0a48b6170e2b34853c189a0d1c41950ae9
MazingerDev <mazinasd7@gmail.com>
2022-12-10 00:32:53 +0200

    olesya and rodion

---
+++ OlesyaAndRodion.java
@@ -0,0 +1,26 @@
+//https://codeforces.com/contest/584/problem/A
+
+import java.util.*;
+
+public class OlesyaAndRodion {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int t = input.nextInt();
+        if (t < 10) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(t);
+            }
+        } else {
+            if (n == 1) {
+                System.out.println(-1);
+                return;
+            }
+            for (int i = 0; i < n-1; i++) {
+                System.out.print(1);
+            }
+            System.out.print(0);
+        }
+    }
+}
